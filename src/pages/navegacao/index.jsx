0a90caa04e0eb60fcaf6a7eb6f9a7816b2348b25
Guilherme Martins <fellipini@gmail.com>
2022-10-18 10:00:34 -0300

    update name

--- src/pages/navegacao/index.jsx
+++ src/pages/navegacao/index.jsx
@@ -3,7 +3,7 @@ import Layout from "../../components/Layout";
 export default function Navegacao() {
   return (
     <Layout titulo="Exemplo de Navegação #01">
-      <h1>Navegação #01</h1>
+      <h1>Navegação #01 Padrão</h1>
     </Layout>
   );
 }
